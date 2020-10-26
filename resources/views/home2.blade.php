672ff15570101ef6b77e96d4be5003788b16cd3c
Andreas Adi <49746748+AndreasAdi@users.noreply.github.com>
2020-10-26 17:43:41 +0700

    tampilan

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -36,13 +36,13 @@ home
 <div class="container mt-5">
 
     <form class="d-flex mx-auto mb-5 col-6">
-        <input class="form-control mr-2 align-middle" type="search" placeholder="Cari Barang" aria-label="Search">
+        <input class="form-control mr-2 align-middle shadow-sm" type="search" placeholder="Cari Barang" aria-label="Search">
         <button class="btn btn-success" type="submit">Cari</button>
       </form>
       <h2>Featured Item</h2>
       <div class='d-flex justify-content-center  flex-wrap'>
         @foreach ($dataBarang as $item)
-            <div class="card m-3" style="width: 18rem;">
+            <div class="card m-3 shadow" style="width: 18rem;">
             <img style="height: 200px;object-fit: scale-down;"  src="{{asset("/storage/images/".$item->gambar_barang)}}" class="card-img-top" alt="...">
                 <div class="card-body">
                 <p class="card-text text-truncate">{{$item->nama_barang}}</p>
