b9edac4f2cd0de67711f935c281bbed501f0480a
Andreas Adi <49746748+AndreasAdi@users.noreply.github.com>
2020-10-30 14:07:04 +0700

    detail, your item ganti ke card

--- resources/views/detailBarang.blade.php
+++ resources/views/detailBarang.blade.php
@@ -8,7 +8,7 @@
         <form action="{{url('barang/addToCart')}}" method="POST">
             @method('POST')
             @csrf
-            <div class="mr-5" style="height: 400px;width:400px;">
+            <div class="mx-auto" style="height: 400px;width:400px;">
                 <img style="height: 400px;width:400px; object-fit: contain;" src="{{asset("/storage/images/".$barang->gambar_barang)}}" alt="">
             </div>
             <div class="m-3">
@@ -17,16 +17,22 @@
                 <input type="hidden" name='nama' value="{{$barang->nama_barang}}">
                 <input type="hidden" value="{{$barang->harga}}" name='harga'>
                 <input type="hidden" value="{{$barang->stok}}" name='stok'>
-                <h3>{{$barang->nama_barang}}</h3>
+                <h4>{{$barang->nama_barang}}</h4>
                 <hr>
-                    <h4>Rp. {{number_format($barang->harga),2,",","."}}</h4>
+                    <h3>Rp. {{number_format($barang->harga),2,",","."}}</h3>
                 <hr>
-                    <h4>Stock : {{$barang->stok}}</h4>
+                    <b>Sisa Stock : {{$barang->stok}}</b>
                 <hr>
-                    Jumlah:<input type="number" name='jumlah'>
+
+                <div class="input-group mb-3" style = "width : 300px">
+                    <input class="form-control col-xs-2" type="number" name='jumlah' placeholder="Jumlah">
+                    <button class="btn btn-success" type="submit" id="button-addon2">Add To Cart</button>
+                  </div>
                 <hr>
-                <button class="btn btn-success" type="submit">Add To Cart</button>
-                <a class="btn btn-primary" href="{{url('user/makeChatroom')}}">Chat Merchant</a>
+
+                <a class="btn btn-danger mb-5" href="{{"/addToWishlist"}}">Add to Wishlist</a>
+                <a class="btn btn-primary mb-5" href="{{url('user/makeChatroom')}}">Chat Merchant</a><br>
+                <b class="mt-5">Deskripsi {{$barang->nama_barang}}</b>
                 <p>{!!$barang->deskripsi_barang!!}</p>
             </div>
         </form>
