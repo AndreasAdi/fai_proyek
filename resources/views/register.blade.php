67373bd2c0b0f8582c3f2783e2b833e2f33029d6
Calista368 <Calistaa368@users.noreply.github.com>
2020-11-02 18:10:38 +0700

    Update register.blade.php
    register add link to login (already sign up)

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -37,6 +37,7 @@
               <label>Confirm Password</label>
               <input name="kpassword" type="password" class="form-control">
             </div>
+            <a href="{{url('/')}}" class="text-primary mt-5">Already Sign Up</a>
             <button type="submit" class="btn btn-block btn-primary">Register</button>
           </form>
     </div>
