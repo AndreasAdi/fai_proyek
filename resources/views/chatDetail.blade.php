ea9d78aa61aa48edadf929ad057e77a6ca102540
Andreas Adi <49746748+AndreasAdi@users.noreply.github.com>
2020-11-14 16:45:21 +0700

    perbaikan tampilan chat

--- resources/views/chatDetail.blade.php
+++ resources/views/chatDetail.blade.php
@@ -4,49 +4,64 @@ Chat Room
 @endsection
 @section('isi')
 @include('alert')
-        <div class="container mt-5 col-6">
+        <div class="container mt-5 col-6 p-5 shadow">
             @if ($id_sender==Session::get('userId'))
                 <h1 class='text-success'>{{$namaRecepient}}</h1>
             @else
                 <h1 class='text-success'>{{$namaSender}}</h1>
             @endif
-            <form action='{{url("user/insertDetail")}}' method="POST">
-                @method('POST')
-                @csrf
-                <input type="hidden" name='idChatroom' value="{{$idChatroom}}">
-                @if (isset($detailChat))
-                    @foreach ($detailChat as $item)
-                        @if (Session::get('userId')==$item['id_user'])
-                            <div class="container bg-success">
-                                @if ($id_sender==$item['id_user'])
-                                    <p class="text-light">Sender</p>
-                                    <p class="text-light">{{$namaSender}}</p>
-                                    <p class="text-light">{{$item['chat']}}</p>
-                                @else
-                                    <p class="text-light">Recepient</p>
-                                    <p class="text-light">{{$namaRecepient}}</p>
-                                    <p class="text-light">{{$item['chat']}}</p>
-                                @endif
-                            </div>
-                        @else
-                            <div class='container bg-primary'>
-                                @if ($id_sender==$item['id_user'])
-                                    <p class="text-light">Sender</p>
-                                    <p class="text-light">{{$namaSender}}</p>
-                                    <p class="text-light">{{$item['chat']}}</p>
-                                @else
-                                    <p class="text-light">Recepient</p>
-                                    <p class="text-light">{{$namaRecepient}}</p>
-                                    <p class="text-light">{{$item['chat']}}</p>
-                                @endif
-                            </div>
-                        @endif
-                    @endforeach
-                @else
-                    <p>Tidak Ada Chat</p><br>
-                @endif
-                <input type="text" name='message' class="form-control">
-                <button type="submit" class="btn btn-success">Send</button>
+            <hr>
+            <div style="height: 250px;overflow-y: scroll;" class="container mt-5">
+                <form action='{{url("user/insertDetail")}}' method="POST">
+                    @method('POST')
+                    @csrf
+                    <input type="hidden" name='idChatroom' value="{{$idChatroom}}">
+                    @if (isset($detailChat))
+                        @foreach ($detailChat as $item)
+                            @if (Session::get('userId')==$item['id_user'])
+                                <div class="d-flex align-items-end flex-column bd-highlight mb-3">
+                                    @if ($id_sender==$item['id_user'])
+                                    <div style="background-color: #97c9b2" class="pl-4 pr-4 pt-2 col col-md-auto bd-highlight col-2  rounded-pill">
+                                        <p class="text-light">{{$item['chat']}}</p>
+                                    </div>
+                                    @else
+                                    <div  class="pl-4 pr-4 pt-2 col col-md-auto bd-highlight col-2  rounded-pill  bg-success bg-gradient">
+                                        <p class="text-light">{{$item['chat']}}</p>
+                                    </div>
+                                    @endif
+                                </div>
+
+                            @else
+                                    <div class="d-flex align-items-start flex-column bd-highlight mb-3">
+                                        @if ($id_sender==$item['id_user'])
+                                        <div class="pl-4 pr-4 pt-2 bd-highlight col col-md-auto rounded-pill bg-secondary bg-gradient">
+                                            <p class="text-light text-center">{{$item['chat']}}</p>
+
+                                        </div>
+                                        <br>
+                                        @else
+                                        <div class="pl-4 pr-4 pt-2 bd-highlight col col-md-auto bg-primary rounded-pill float-left">
+                                            <p  class="text-light text-center">{{$item['chat']}}</p>
+
+                                        </div>
+                                        <br>
+                                        @endif
+                                    </div>
+
+                            @endif
+                        @endforeach
+                    @else
+                        <p>Tidak Ada Chat</p><br>
+                    @endif
+
+            </div>
+                <hr>
+                <div class="input-group mt-5 mb-3">
+                    <input type="text" class="form-control" placeholder="Masukkan Pesan" aria-label="Recipient's username" aria-describedby="button-addon2" name="message">
+                    <button class="btn btn-success bg-gradient" type="submit" id="button-addon2">Send</button>
+                  </div>
+                {{-- <input type="text" name='message' class="form-control">
+                <button type="submit" class="btn btn-success">Send</button> --}}
             </form>
         </div>
     @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,53 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css"
+        integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
     <style>
         @yield('style');
+
     </style>
     <title>@yield('judul')</title>
 </head>
+
 <body>
     <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-success shadow">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{url('user/home')}}"> <h3> E-Store</h3> </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                @if (session()->get("isMerchant")==true)
-                    <li>{{-- nanti di hide kalau loginnya bukan akun merchant--}}
-                        <a class="nav-link" href="{{url('barang/addItem')}}">Add An Item</a>
+            <a class="navbar-brand" href="{{url('user/home')}}">
+                <h3> E-Store</h3>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    @if (session()->get("isMerchant")==true)
+                        {{-- nanti di hide kalau loginnya bukan akun merchant--}}
+                    <li class="nav-item dropdown float-right">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                          Menu Merchant
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a class="dropdown-item" href="{{url('barang/addItem')}}">Add An Item</a>
+                            </li>
+                            <li> <a class="dropdown-item" href="{{url('barang/yourItem')}}">Your Item</a></li>
+                            <li>
+                                <a class="dropdown-item" href="{{url('user/penjualan')}}">Daftar Penjualan</a>
+                            </li>
+
+                        </ul>
+                      </li>
+                      <li>
+                        <a class="nav-link" href="{{url('user/pembelian')}}">Daftar Pembelian</a>
                     </li>
-                <li> <a class="nav-link" href="{{url('barang/yourItem')}}">Your Item</a></li>
-                <li>
-                    <a class="nav-link" href="{{url('user/penjualan')}}">Daftar Penjualan</a>
-                </li>
-                <li>
-                    <a class="nav-link" href="{{url('user/pembelian')}}">Daftar Pembelian</a>
-                </li>
-                @else
+                    @else
                     @if (session()->get('isAdmin')==false)
-                        <li>
-                            <a class="nav-link" href="{{url('user/regisMerchant')}}">Register As A Merchant</a>
-                        </li>
-                        <li>
-                            <a class="nav-link" href="{{url('user/pembelian')}}">Daftar Pembelian</a>
-                        </li>
+                    <li>
+                        <a class="nav-link" href="{{url('user/regisMerchant')}}">Register As A Merchant</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="{{url('user/pembelian')}}">Daftar Pembelian</a>
+                    </li>
                     @endif
-                @endif
-                <li>
-                    <a class="nav-link" href="{{url('user/alamat')}}">Alamat</a>
-                </li>
-                <a class="nav-link" href="{{url('user/wishlist')}}">Wishlist</a>
-                @if (session()->get("isAdmin")==true)
+                    @endif
+                    <li>
+                        <a class="nav-link" href="{{url('user/alamat')}}">Alamat</a>
+                    </li>
+                    <a class="nav-link" href="{{url('user/wishlist')}}">Wishlist</a>
+                    @if (session()->get("isAdmin")==true)
                     <li> <a href="{{url('voucher/addVoucher')}}" class="nav-link">Voucher</a></li>
                     <li>
                         <a class="nav-link" href="{{url('sale/addSale')}}">Sale</a>
@@ -58,34 +75,43 @@
                     <li>
                         <a class="nav-link" href="{{url('admin/listSale')}}">Lihat Sale</a>
                     </li>
-                @else
-                    <li> <a href="{{url('barang/cart')}}" class="nav-link">Cart</a></li>
+                    @else
+
                     <li>
                         <a class="nav-link" href="{{url('user/loadChatroom')}}">Chat Room</a>
                     </li>
-                    
+
                     <li>
                         <a class="nav-link" href="{{url('user/listVoucher')}}">Lihat Voucher</a>
                     </li>
                     <li>
                         <a class="nav-link" href="{{url('user/listSale')}}">Lihat Sale</a>
                     </li>
-                @endif
-            </ul>
-          </div>
-          <div class="form-inline my-2 my-lg-0">
-            <a href="{{url('user/prosesLogout')}}" class="btn btn-danger">Logout</a>
+
+                    <li> <a href="{{url('barang/cart')}}" class="nav-link ml-auto">Cart</a></li>
+                    @endif
+                </ul>
+            </div>
+
+            <div class="form-inline my-2 my-lg-0">
+
+                <a href="{{url('user/prosesLogout')}}" class="btn btn-danger">Logout</a>
+            </div>
         </div>
-        </div>
-      </nav>
-      @yield('isi')
+    </nav>
+    @yield('isi')
 
     <!-- JavaScript Bundle with Popper.js -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js"
+        integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous">
+    </script>
 
     @yield('script')
 
 </body>
+
 </html>
