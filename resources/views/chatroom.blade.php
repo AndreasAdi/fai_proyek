5e594d8c8a30303e591b659709061b5f5bef7c5f
Andreas Adi <49746748+AndreasAdi@users.noreply.github.com>
2020-11-15 21:34:04 +0700

    perbaikan chat

--- resources/views/chatroom.blade.php
+++ resources/views/chatroom.blade.php
@@ -14,9 +14,9 @@ Chat Room
     @foreach ($headerChat as $key => $item)
 
         <div class="shadow rounded p-3 mb-3">
-           <b> {{$nama[$key]['nama_user']}}</b>
-            <span class="ml-3 text text-secondary"> {{$item->last_message}}</span>
-            <a href="{{url("user/loadDetailChat/$item[id_chatroom]")}}" class="btn btn-primary float-right">View Chat</a>
+           <b> {{$nama[$key]['nama_user']}}</b><br>
+            <span class="text text-secondary"> {{$item->last_message}}</span>
+            <a href="{{url("user/loadDetailChat/$item[id_chatroom]")}}" class="btn btn-success float-right">View Chat</a>
         </div>
 
 
